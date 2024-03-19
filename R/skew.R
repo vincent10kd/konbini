666306dd9073d3f4aec869d070606e5bbc6573b1
Arina ten Cate - Hoek <arinetencate-hoek@Arinas-Air-2.fritz.box>
2024-03-19 23:05:12 +0100

    new stuff

---
+++ R/skew.R
@@ -0,0 +1,16 @@
+#' @title Calculate skewness of a vector
+#'
+#' @description Calculates the skewness of a continuous variable
+#'
+#' @param x A vector.
+#'
+#' @return Returns the skewness of the input vector.
+#' @examples
+#' skewness(iris$Sepal.Length)
+#' @export
+#'
+
+skew <- function(x){
+  (sum((x - mean(x))^3)/length(x))/(sum((x - mean(x))^2)/length(x))^(3/2)
+}
+
